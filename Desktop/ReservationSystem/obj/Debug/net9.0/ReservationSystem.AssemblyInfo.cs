9c1e24cdd1ce9ba8ee03585bb48c9288faa4ea51
Kardelen Biçen <kardelen.bicen.tr@gmail.com>
2025-07-14 16:02:20 +0300

    days start monday

--- Desktop/ReservationSystem/obj/Debug/net9.0/ReservationSystem.AssemblyInfo.cs
+++ Desktop/ReservationSystem/obj/Debug/net9.0/ReservationSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6597cda6231ba40b3649ccf2f91924e209494173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798d1534675dd062ecc6d45595c5f2ec5cf43738")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
