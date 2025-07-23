c9675b1646e1c04dfc19ab315d20fbe4ec959ad9
Kardelen Biçen <kardelen.bicen.tr@gmail.com>
2025-07-23 18:41:08 +0300

    fix migrations

--- Desktop/ReservationSystem/obj/Debug/net9.0/ReservationSystem.AssemblyInfo.cs
+++ Desktop/ReservationSystem/obj/Debug/net9.0/ReservationSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b69ddcc5f58ee86e1b6f0c337a22a73db14dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71002256b0de99f2fa4f94a8e12854842c56efcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
