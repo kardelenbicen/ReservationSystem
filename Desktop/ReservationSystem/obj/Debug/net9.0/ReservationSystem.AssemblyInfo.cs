968f30dc6b50e166fd2cd0b17f26e1124c14a6ab
Kardelen Biçen <kardelen.bicen.tr@gmail.com>
2025-07-23 19:49:25 +0300

    fix turkish character

--- Desktop/ReservationSystem/obj/Debug/net9.0/ReservationSystem.AssemblyInfo.cs
+++ Desktop/ReservationSystem/obj/Debug/net9.0/ReservationSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71002256b0de99f2fa4f94a8e12854842c56efcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9675b1646e1c04dfc19ab315d20fbe4ec959ad9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
