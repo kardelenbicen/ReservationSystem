a1bcaa79bb0bb99adc9b2a7ca4925b6124b6adc2
Kardelen Biçen <kardelen.bicen.tr@gmail.com>
2025-07-31 14:13:09 +0300

    fixed edit meetingrooms

--- Desktop/ReservationSystem/obj/Debug/net9.0/ReservationSystem.AssemblyInfo.cs
+++ Desktop/ReservationSystem/obj/Debug/net9.0/ReservationSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c0f2be430d47961ad4df6bf78184254bc80dcb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d95e6ae69beff82f50940aeaee9cc83250ea69")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
